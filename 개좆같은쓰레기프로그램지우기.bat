3a2b039c1b12df3be4daa830b640c716c39faeb2
LightEach <lighteach@gmail.com>
2019-08-02 00:27:47 +0900

    배치 파일 최초 업로드
    배치 파일 최초 업로드

---
+++ 개좆같은쓰레기프로그램지우기.bat
@@ -0,0 +1,52 @@
+@ECHO OFF
+CLS
+ECHO Did you hope to continue?
+PAUSE
+
+ECHO [우리은행 보안로그 수집기]
+"C:\Program Files (x86)\WooriBank\RealIp\uninst.exe"
+ECHO --------------------------------------------------------------------------------
+ECHO [안랩 씨발 안티바이러스]
+"C:\Program Files\AhnLab\Safe Transaction\V3Medic.exe" -Uninstall
+ECHO --------------------------------------------------------------------------------
+ECHO [좆같은 베라포트]
+"C:\Program Files (x86)\Wizvera\Veraport20\unins000.exe"
+ECHO --------------------------------------------------------------------------------
+ECHO [씨발 애니싸인]
+rem "C:\Program Files (x86)\Wizvera\Veraport20\unins000.exe"
+"C:\Program Files (x86)\SoftForum\XecureWeb\AnySign\uninst.exe"
+ECHO --------------------------------------------------------------------------------
+ECHO [개 좆같은 위즈베라]
+"C:\Program Files (x86)\Wizvera\Delfino-G3\unins000.exe"
+"C:\Program Files (x86)\Wizvera\Delfino\unins000.exe"
+rem "C:\Program Files (x86)\Wizvera\Common\wpmsvc\unins000.exe"
+ECHO --------------------------------------------------------------------------------
+ECHO [이노릭스인지 뭔지 하는 개븅신]
+"C:\Program Files (x86)\INNORIX\uninstall\uninstall_innogmp7.exe"
+ECHO --------------------------------------------------------------------------------
+ECHO [엔프로텍트 개 호로새끼]
+"C:\Program Files (x86)\INCAInternet UnInstall\nProtect Online Security\nProtectUninstaller.exe"
+ECHO --------------------------------------------------------------------------------
+ECHO [엔프로텍트 네티즌인가 뭔가 하는 병신]
+"C:\Program Files (x86)\INCAInternet UnInstall\nProtect Netizen v5.5\npenUninstall5.exe"
+ECHO --------------------------------------------------------------------------------
+ECHO [개븅신 크로쓰엑쓰]
+"C:\Program Files (x86)\iniLINE\CrossEX\crossex\uninst.exe"
+"C:\Program Files (x86)\iniLINE\CrossEX\crossex\UnInstallCrossEXLocal.exe"
+ECHO --------------------------------------------------------------------------------
+ECHO [처음보는 별 개잡것]
+"C:\Program Files (x86)\espider\espiderWin\uninst.exe"
+ECHO --------------------------------------------------------------------------------
+ECHO [쓰레기 TouchEn]
+"C:\Program Files\RaonSecure\NxWeb\uninst_32.exe"
+ECHO --------------------------------------------------------------------------------
+ECHO [도대체 뭐하는 새낀지 알 수가 없는 씨발놈]
+"C:\Program Files (x86)\MarkAny\EPSBroker\Uninst_MaEPSBroker.exe"
+ECHO --------------------------------------------------------------------------------
+ECHO [Crevit 인지 뭔지 하는 이상한 새끼]
+"C:\Program Files (x86)\NTUMNETWORKS\CrevitEx\unins000.exe"
+ECHO --------------------------------------------------------------------------------
+ECHO [드디어 찾은 Interezen 개새끼]
+"C:\Program Files (x86)\IPinside_LWS\I3GSvcManager.exe" /uninstall
+
+
